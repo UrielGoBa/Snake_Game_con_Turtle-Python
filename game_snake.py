62b363e6de083dccc05767516c751167f579fac4
Uriel <urielgonzalezbarranco@gmail.com>
2022-09-20 17:59:07 -0500

    Definición de pantalla de juego

--- game_snake.py
+++ game_snake.py
@@ -1,3 +1,17 @@
 import turtle
 import time
 import random
+
+#Creacion del espacio del juego
+
+pantalla = turtle.Screen() # Creación de la pantalla
+pantalla.setup(750,750) #Tamaño de la pantalla
+pantalla.bgcolor('#fef') #Color de fondo
+pantalla.title('Snake Game') #Titulo de la pantalla
+
+#Creación del jugador
+
+
+
+
+turtle.done()
