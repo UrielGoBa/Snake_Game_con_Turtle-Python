be45edc50850e26423d748f460d43bb4e6b082d5
Uriel <urielgonzalezbarranco@gmail.com>
2022-09-21 16:27:06 -0500

    Colisiones con la pantalla y el cuerpo

--- game_snake.py
+++ game_snake.py
@@ -29,6 +29,8 @@ comida.penup()
 comida.goto(0,100)
 comida.speed(0)
 
+cuerpo = [] #Incremento del cuerpo, se usa en While de movimiento
+
 #Movimiento de la serpiente
 
 #Movimiento por teclado
@@ -74,12 +76,54 @@ pantalla.onkeypress(derecha, "Right")
 while True:
     serpiente._update() # Actualización de la posición de la serpiente
     
-    if serpiente.distance(comida) < 20:
+    #En caso de tocar uno de los bordes de si mismo
+    
+    if serpiente.xcor() > 300 or serpiente.xcor() < -300 or serpiente.ycor() > 300 or serpiente.ycor() < -300:
+        time.sleep(1)
+        for i in cuerpo:
+            i.clear()
+            i.hideturtle()
+        serpiente.home()
+        serpiente.direction = 'stop'
+        cuerpo.clear()
+
+    if serpiente.distance(comida) < 20: #Cuando la serpiente toca la tortuga, ésta, reaparece en otro lugar random
         x = random.randrange(-300, 300, 20)
         y =  random.randrange(-300, 300, 20)
         comida.goto(x, y)
+        nuevo_cuerpo = turtle.Turtle()#Creacion del incremento de la serpiente
+        nuevo_cuerpo.shape('circle')
+        nuevo_cuerpo.color('#449C4A')
+        nuevo_cuerpo.penup()
+        nuevo_cuerpo.goto(0,0)
+        nuevo_cuerpo.speed(0)
+        cuerpo.append(nuevo_cuerpo) #Se agrega la nueva parte del cuerpo en la lista 'cuerpo'
+
+    #Vamos a agregar el cuerpo a la serpiente
+
+    total = len(cuerpo)
+
+    for index  in range(total -1, 0, -1):
+        x = cuerpo[index-1].xcor()
+        y = cuerpo[index-1].ycor()
+        cuerpo[index].goto(x,y)
+
+    if total > 0: #El primer elemento creado, no sigue a otro elemento similar, si no a la serpiente principal
+        x = serpiente.xcor() #Se obtienen las coodenadas del la serpiente principal
+        y = serpiente.ycor()
+        cuerpo[0].goto(x, y) #Se envía el primer elemento a las coordenadas de la serpiente principal
 
     movimiento()# Movimiento de la serpiente
+
+    for i in cuerpo:
+        if i.distance(serpiente) < 20:
+            for i in cuerpo:
+                i.clear()
+                i.hideturtle()
+            serpiente.home()
+            cuerpo.clear()
+            serpiente.direction= 'stop'
+
     time.sleep(retraso) #Retraso del movimiento
     #Repetición
 
